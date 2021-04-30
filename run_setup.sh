fb43956ec4aade84b5b96cd64380fec8ed9903e7
timo-piirmann <timurlane5@gmail.com>
2021-04-30 11:37:02 +0300

    Update prebuilt container file links

--- run_setup.sh
+++ run_setup.sh
@@ -7,9 +7,9 @@ mkdir -p userdir
 mkdir -p outdata
 
 # Download itsx_plutof.sif
-echo "Downloading ITSx singularity container from https://plutof.ut.ee/#/filerepository/view/3576413 ...\n"
-wget https://files.plutof.ut.ee/public/orig/C5/BA/C5BA922996A4243C30F0B4BBCC6F7DAA3643FC6CB99CF6851C3891CD57E34DA5
-mv C5BA922996A4243C30F0B4BBCC6F7DAA3643FC6CB99CF6851C3891CD57E34DA5 itsx_plutof.sif
+echo "Downloading ITSx singularity container from https://plutof.ut.ee/#/filerepository/view/3950594 ...\n"
+wget https://files.plutof.ut.ee/public/orig/8F/BC/8FBCA0F8DE48F0039394BC0E2E984BEF71100D219D0ADA287A66EC3B4DB6A67B
+mv 8FBCA0F8DE48F0039394BC0E2E984BEF71100D219D0ADA287A66EC3B4DB6A67B itsx_plutof.sif
 
 # end here
 echo "Done"
